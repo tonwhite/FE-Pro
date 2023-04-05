cfdfd93d0607286c3e14d9fc8f522575e027370f
Anton Glushchenko <anton.glushchenko@gmail.com>
2023-04-05 15:51:31 +0300

    created working function, random count and checks

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ console.log(array);
 
 */
 
+const array = [1, 2, 3, 4, 5, 6, 7];
+console.log(array, `до функції`);
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
+    } else {
+        console.log(`Дане значення відсутнє в масиві`);
+    }
+    return array;
+}
+const n = Math.floor(Math.random() * 10);
+console.log(n, `Рандомне значення`);
+removeElement(array, n);
+console.log(array,`після функції`);
