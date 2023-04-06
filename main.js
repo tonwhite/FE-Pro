355bbedbd599f4065b807ff0252849674cb61c6c
Anton <anton.glushchenko@gmail.com>
2023-04-06 15:22:53 +0300

    made final improvements

--- main.js
+++ main.js
@@ -9,32 +9,39 @@ console.log(array);
 // Результат: [1, 2, 3, 4, 6, 7]
 
 */
-function ForwardCount(array) {
+
+// Створення функції для знаходження  випадкового числа з проміжку
+function getRanNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Створення функції для заповнення масиву випадковими числами від 1 до 10
+function ranFill10(array) {
     const len = array.length;
     for (let i = 0; i < len; i++) {
-        array[i] = i + 1;
+        array[i] = getRanNum(1, 10);
     }
     return array;
 }
-
-const array = new Array(7);
+// Створення масиву випадкової довжини від 5 до 10
+const array = new Array(getRanNum(5,10));
 console.log(array,`пустий масив`);
-
-ForwardCount(array);
+// Заповнення масиву випадковими числами від 1 до 10 
+ranFill10(array);
 console.log(array, `заповнений масив`);
-
+// Створення функції видалення елементу
 function removeElement(array, item) {
-    const index = array.indexOf(item);
-    if (index !== -1) {
-        array.splice(index, 1);
-    } else {
-        console.log(`Дане значення відсутнє в масиві`);
+    const length = array.length;
+    for (let i = 0; i < length; i++) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+            i--;
+        }
     }
     return array;
 }
-
-const n = Math.ceil(Math.random() * 10);
+// Генерація випадкового числа для видалення з масиву
+const n = getRanNum(1, array.length - 1);
 console.log(n, `рандомне значення`);
-
+// Видалення випадкового елементу
 removeElement(array, n);
 console.log(array,`поранений масив`);
