d5e0fc815eb5f6657beec841990f924da1956734
Anton <anton.glushchenko@gmail.com>
2023-04-10 15:19:06 +0300

    2 task completed

--- main.js
+++ main.js
@@ -26,11 +26,24 @@ function filter(input, isEvenCb) {
 Реалізувати функцію copy(list) копіювання масиву.
 
 Передбачити можливість передачі другим аргументом функції. При копіюванні масиву - функція застосовується до кожного елемента масиву, що копіюється.
-
 newL = copy(list, (value) => value * 10 )
 */
 
+function copy(list, callBack) {
+    const length = list.length;
+    const array = [];
 
+    for (let i = 0; i < length; i++) {
+        const item = callBack ? callBack(list[i]) : list[i];
+        array.push(item);
+    }
 
+    return array;
+}
 
+const list = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 9, 0];
+console.log(copy(list));
+
+newL = copy(list, (value) => value * 10);
+console.log(newL);
 
