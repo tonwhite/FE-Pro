ffa03ef5efc8b14bcb75723348caa2678d5d9b0b
Anton <anton.glushchenko@gmail.com>
2023-04-10 14:38:16 +0300

    1 task completed

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 /*
- Задача - 1
+Задача - 1
 Напиши функцію filter, яка приймає функцію зворотного виклику та масив. Повертає вона масив значень, котрим ф-я зворотного виклику поверне true.
+*/
 
 const input = [1, 2, 3, 4, 5, 6];
- function isEven(x) { return x % 2 == 0; }
- console.log(filter(input, isEven)); // [2, 4, 6]
- */
+function isEven(x) { return x % 2 == 0; }
+console.log(filter(input, isEven)); // [2, 4, 6]
 
+function filter(input, isEvenCb) {
+    const array = [];
+    const length = input.length;
+    
+    for (let i = 0; i < length; i++) {
+        if (isEvenCb(input[i])) {
+            array.push(input[i]);
+        };
+    }
+
+    return array;
+}
 
 /*
 Задача - 2
