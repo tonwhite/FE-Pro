89daad4aec7ab58362fe5e068c17ab1242ff627a
Anton <anton.glushchenko@gmail.com>
2023-04-15 22:46:33 +0300

    final

--- main.js
+++ main.js
@@ -5,3 +5,19 @@
 
 [1, 2, 3, 3.1, 4, 5 , 6, 7, 8] */
 
+function flat(array) {
+    let result = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            result = result.concat(flat(array[i]));
+        } else {
+            result.push(array[i]);
+        }   
+    }
+    return result;
+}
+
+const mass = [[1, 2, 3, [3.1]], 4, [5, 6, [7, 8]]];
+const flatMass = flat(mass);
+console.log(flatMass);
