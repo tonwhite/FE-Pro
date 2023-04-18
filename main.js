1a61c0b284d47f3bdd87ba70eb3d0ec2bf4152c6
Anton <anton.glushchenko@gmail.com>
2023-04-18 17:15:55 +0300

    final

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 /*
- 
- Умова задачі
- 
- 
- 
+Створити об'єкт з такою структурою:
+
+const obj = { a: 10, b: 12, c: { f: 13, g: { m: 20} } } 
+Написати ф-ю convert(obj), він одержує аргументом obj. Функція повинна повернути новий об'єкт:
+
+const newObj = { a: 10, b: 12, f: 13, m: 20 }
  */
+
+const obj = { a: 10, b: 12, c: { f: 13, g: { m: 20 } } };
+
+function convert(obj) {
+  let newObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      let subObj = convert(obj[key]);
+      Object.assign(newObj, subObj);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+console.log(convert(obj));
