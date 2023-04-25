90e91c38141c3532dd63e9ea529e2c343e3b5a3f
Anton <anton.glushchenko@gmail.com>
2023-04-25 17:31:38 +0300

    final optimization

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-/*
- 
- Умова задачі
- 
- 
- 
- */
-
-function displayProperties(obj) {
-  for (const key in obj) {
-    if (key !== 'renderObject' && obj.hasOwnProperty(key)) {
-      document.write(`${key}: ${obj[key]}<br>`);
-    }
-  }
-}
+// Задача - 1
+// Задано об'єкт з будь-якою кількістю властивостей. Однією з властивостей є функція renderObject(), яка описана у window.
+// При виклику методу obj.renderObject() -> виводить у document весь вміст об'єкта, крім самого методу renderObject
 
 window.renderObject = function () {
-  displayProperties(this);
+  let html = '';
+
+  for (const key in obj) {
+    if (key !== 'renderObject' && this.hasOwnProperty(key)) {
+      html += `${key}: ${this[key]}</br>`;
+    }
+  }
+
+  document.write(html);
 };
 
 const obj = {
@@ -26,6 +22,10 @@ const obj = {
 };
 
 obj.renderObject();
+
+// Задача - 2
+// В об'єкті data існує метод addRecord, який аргументами отримує будь-який набір об'єктів.
+// Метод addRecord додає всі властивості переданих об'єктів data.
 
 function mergeObjects(target, ...objects) {
   objects.forEach((obj) => {
@@ -42,34 +42,10 @@ const data = {
   y: -50,
 };
 
-data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50});
+data.addRecord({ x: 10 }, { y: 20 }, { z: 30, x: 50 });
 
 console.log(data.x); // 50
 console.log(data.y); // 20
 console.log(data.z); // 30
 console.log(data.p); // 600
 console.log(data.str); // 'hello'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
