e1c93ab04605d9200020055481effc0dc1a0f4ee
Anton <anton.glushchenko@gmail.com>
2023-05-04 15:16:58 +0300

    final

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-/*
- 
- Умова задачі
- 
- 
- 
- */
+/* Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+sum(3) = 3
+sum(5) = 8
+sum(20) = 28 */
+
+function getSum(num) {
+  let result = 0;
+  
+  return function (num) {
+    result += num;
+    return result;
+  }
+}
+
+const sum = getSum();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
