86fdb6665fbc551f04a3493f427344361808d5d9
Anton <anton.glushchenko@gmail.com>
2023-05-17 14:24:40 +0300

    set technical task

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 /*
- 
- Умова задачі
- 
- 
- 
- */
+Практикуємось з використанням генераторів
+
+Реалізувати ф-ю виду:
+
+function* getPartsOfWord (string) {
+  ....
+}
+
+const iterator = getPartsOfWord("hello");
+
+Таку, щоб виклики:
+
+iterator.next()
+
+Отримати такі результати:
+
+{value: 'h', done: false}
+{value: 'he', done: false}
+{value: 'hel!', done: false}
+{value: 'hell', done: false}
+{value: 'hello@', done: false}
+{value: 'hello', done: true}
+
+Символи: !, @ повинні передаватись у якості формальних параметрів у iterator.next()
+*/
