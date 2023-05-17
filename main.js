e7fe6f79b1202382c3ab6d0ddb8609d397fbc7a0
Anton <anton.glushchenko@gmail.com>
2023-05-17 18:02:11 +0300

    created iterator and received desired output

--- main.js
+++ main.js
@@ -24,3 +24,34 @@ iterator.next()
 
 Символи: !, @ повинні передаватись у якості формальних параметрів у iterator.next()
 */
+
+function* getPartsOfWord(string) {
+    let word = '';
+    let specialCharacter = '';
+    const format = /[ !@ ]/;
+
+    for (let i = 0; i < string.length; i++) {
+        word += string[i];
+        if (specialCharacter) {
+            word += specialCharacter;
+            specialCharacter = '';
+        }
+        
+        specialCharacter = yield word;
+        if (format.test(word)) {
+            word = word.slice(0, -1);
+        }
+    }
+    return word;
+}
+
+
+const iterator = getPartsOfWord("hello");
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next('!'));
+console.log(iterator.next());
+console.log(iterator.next('@'));
+console.log(iterator.next());
+
