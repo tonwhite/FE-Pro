1b21a3be3d2b84b6341d35f75d5d6675cd8d5fa0
Anton <anton.glushchenko@gmail.com>
2023-06-28 14:18:27 +0300

    very cute

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-/**/
+// У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg Вивести зображення з цієї папки, отримане випадковим чином (Math.random)
 
+const randomNumber = Math.floor(Math.random() * 9) + 1;
 
+const img = document.createElement('img');
 
+const imagePath = `./img/${randomNumber}.jpg`
+console.log(imagePath, "imagePath");
 
+img.src = imagePath;
 
-
-
-
-
+document.body.appendChild(img);
 
 
 
