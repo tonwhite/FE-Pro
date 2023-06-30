b197f0450b08bfc7c6ba6fdf761584e2f18f74d7
Anton <anton.glushchenko@gmail.com>
2023-06-30 22:03:39 +0300

    final edition

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-/**/
+// Есть 3 input.Выводить в textarea содержимое всех полей ввода через запятую.Использовать setInterval.
+// Кожні N - sec перевіряти інпути(input.value) і якщо є зміни, то додавати в textarea
 
+const inputs = Array.from(document.querySelectorAll('input'));
+const textarea = document.getElementById('output');
 
+let previousValues = getValues();
 
+setInterval(checkInputs, 3000);
 
+function getValues() {
+    if (!inputs.length) {
+        console.error('No inputs found');
+        return;
+    }
+    return inputs.map(input => input.value);
+}
 
+function checkInputs() {
+    const currentValues = getValues();
 
+    if (JSON.stringify(currentValues) !== JSON.stringify(previousValues)) {
+        updateTextarea(currentValues);
+        previousValues = [...currentValues];
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function updateTextarea(values) {
+    if (!textarea) {
+        console.error('Textarea not found');
+        return;
+    }
+    textarea.value = values.join(', ');
+}
