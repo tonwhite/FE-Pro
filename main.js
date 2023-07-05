eda3b9bfccab9b9559575c3aea26599d8f3ec455
Anton <anton.glushchenko@gmail.com>
2023-07-05 19:17:12 +0300

    decomposed code to be clear and declarative

--- main.js
+++ main.js
@@ -1,71 +1,105 @@
-// // * Зверстати таблицю 5х5 з будь-яким текстовим вмістом заздалегідь заповненим.
+// * Зверстати таблицю 5х5 з будь-яким текстовим вмістом заздалегідь заповненим.
 // * При кліку на будь - яку ячейку таблиці з'являється всередині ячейки багаторядкове текстове поле(textarea) з текстом який був у ячейці (на яку натиснули) і дві кнопки під нею save, cancel
 // save - зберегти в поточну клікнуту ячйку введені "змінені данні", cancel - залишить все без змін як було раніше.
 //     P.S.обов'язково використати делегування події.
 
+// Функція створення елементу
+function createElement(type, properties = {}) {
+    const element = document.createElement(type);
+    Object.entries(properties).forEach(([key, value]) => {
+        if (key === 'dataset') {
+            Object.entries(value).forEach(([dataKey, dataValue]) => {
+                element.dataset[dataKey] = dataValue;
+            });
+        } else {
+            element[key] = value;
+        }
+    });
+    return element;
+}
+
+// Функції для створенням таблиці
 function createCell(textContent) {
-    const td = document.createElement('td');
-    td.textContent = textContent;
-    return td;
+    return createElement('td', { textContent, dataset: { type: 'cell' } });
 }
 
 function createRow(cells) {
-    const tr = document.createElement('tr');
+    const tr = createElement('tr');
     cells.forEach(cell => tr.appendChild(cell));
     return tr;
 }
 
 function createTable(rows) {
-    const table = document.createElement('table');
-    table.className = 'table';
+    const table = createElement('table', { className: 'table' });
     rows.forEach(row => table.appendChild(row));
     return table;
 }
 
-function clearCell(cell) {
-    cell.innerHTML = cell.dataset.oldValue;
-}
+const TABLE_SIZE = 5;
 
-function createEditor(cell) {
-    cell.dataset.oldValue = cell.textContent;
-
-    const textarea = document.createElement('textarea');
-    textarea.value = cell.textContent;
-
-    const saveBtn = document.createElement('button');
-    saveBtn.textContent = 'Save';
-    saveBtn.className = 'save-btn';
-
-    const cancelBtn = document.createElement('button');
-    cancelBtn.textContent = 'Cancel';
-    cancelBtn.className = 'cancel-btn';
-
-    cell.innerHTML = '';
-    cell.append(textarea, saveBtn, cancelBtn);
-}
-
+// Створення таблиці з заданим розміром
 const table = createTable(
-    Array(5).fill(0).map((_, i) =>
+    Array.from({ length: TABLE_SIZE }, (_, i) =>
         createRow(
-            Array(5).fill(0).map((_, j) => createCell(`Cell ${i * 5 + j + 1}`))
+            Array.from({ length: TABLE_SIZE }, (_, j) =>
+                createCell(`Cell ${i * TABLE_SIZE + j + 1}`)
+            )
         )
     )
 );
 document.body.appendChild(table);
 
-table.addEventListener('click', function (event) {
-    const target = event.target;
-    const isCell = target.tagName === 'TD';
-    const isSaveBtn = target.classList.contains('save-btn');
-    const isCancelBtn = target.classList.contains('cancel-btn');
+// Функції для редагуванням комірки
+function handleSave(oldValue, cell, textarea) {
+    const saveButton = document.querySelector('.save-btn');
+    const cancelButton = document.querySelector('.cancel-btn');
 
-    if (isCell) {
-        createEditor(target);
-    } else if (isSaveBtn) {
-        const textarea = target.parentElement.querySelector('textarea');
-        target.parentElement.dataset.oldValue = textarea.value;
-        clearCell(target.parentElement);
-    } else if (isCancelBtn) {
-        clearCell(target.parentElement);
+    cell.innerText = textarea.value; // Використання internalText для запобігання потенційним XSS-атакам
+    removeEventListeners(saveButton, cancelButton);
+    cell.dataset.type = 'cell';
+}
+
+function handleCancel(oldValue, cell) {
+    const saveButton = document.querySelector('.save-btn');
+    const cancelButton = document.querySelector('.cancel-btn');
+
+    cell.innerText = oldValue; // Використання internalText для запобігання потенційним XSS-атакам
+    removeEventListeners(saveButton, cancelButton);
+    cell.dataset.type = 'cell';
+}
+
+function removeEventListeners(saveButton, cancelButton) {
+    // Видалиення раніше доданих event listeners
+    saveButton.removeEventListener('click', saveButton.listener);
+    cancelButton.removeEventListener('click', cancelButton.listener);
+}
+
+function createEditor(cell) {
+    const oldValue = cell.textContent;
+    const textarea = createElement('textarea', { value: oldValue, maxLength: 100 });
+    const saveButton = createElement('button', { textContent: 'Save', className: 'save-btn' });
+    const cancelButton = createElement('button', { textContent: 'Cancel', className: 'cancel-btn' });
+
+    // Додавання слухачів за допомогою обробників збереження та скасування
+    saveButton.listener = () => handleSave(oldValue, cell, textarea);
+    cancelButton.listener = () => handleCancel(oldValue, cell);
+
+    saveButton.addEventListener('click', saveButton.listener);
+    cancelButton.addEventListener('click', cancelButton.listener);
+
+    // Очишення комірки і додавання нових елементів редактора
+    cell.innerHTML = '';
+    cell.appendChild(textarea);
+    cell.appendChild(saveButton);
+    cell.appendChild(cancelButton);
+    cell.dataset.type = 'editor';
+    textarea.focus();
+}
+
+// Делегування подій
+table.addEventListener('click', function (event) {
+    const cell = event.target.closest('[data-type="cell"]');
+    if (cell) {
+        createEditor(cell);
     }
 });
